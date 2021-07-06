4d7fb965f018c68ec6ba5e589eab928343fa7a4c
Vivi Cowan <vivicare@gmail.com>
2021-07-05 23:09:50 -0700

    adding js to buttons to navigate to different page

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Navigating to github repository and deployed webpage for weather dashboard.
+$("#wd-repo-btn").on('click', function() {
+	location.href = "https://github.com/vivicowan/weather-dashboard";
+});
+
+$("#wd-web-btn").on('click', function() {
+	location.href = "https://vivicowan.github.io/weather-dashboard/";
+});
+
+
+// Navigating to github repository and deployed webpage for workday scheduler.
+$("#ws-repo-btn").on('click', function() {
+	location.href = "https://github.com/vivicowan/workday-scheduler";
+});
+
+$("#ws-web-btn").on('click', function() {
+	location.href = "https://vivicowan.github.io/workday-scheduler/";
+});
+
+
+// Navigating to github repository and deployed webpage for crypto launch.
+$("#cl-repo-btn").on('click', function() {
+	location.href = "https://github.com/m-car/crypto-launchpad";
+});
+
+$("#cl-web-btn").on('click', function() {
+	location.href = "https://m-car.github.io/crypto-launchpad/";
+});
+
+
+// Navigating to github repository and deployed webpage for coding quiz.
+$("#cq-repo-btn").on('click', function() {
+	location.href = "https://github.com/vivicowan/coding-quiz";
+});
+
+$("#cq-web-btn").on('click', function() {
+	location.href = "https://vivicowan.github.io/coding-quiz/";
+});
